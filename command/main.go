f9e9540d8c278551c0c14a5f2501fe0e2eda0669
BakhodiribnYashinibnMansur <phapp0224mb@gmail.com>
2022-04-18 11:52:00 +0500

    added configs via viper

--- command/main.go
+++ command/main.go
@@ -6,9 +6,16 @@ import (
 	"todocc/package/repository"
 	"todocc/package/service"
 	"todocc/server"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
@@ -17,4 +24,10 @@ func main() {
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
