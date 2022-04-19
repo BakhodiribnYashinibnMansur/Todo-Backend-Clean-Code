416e0bcf27644e58b28e6fa748bea237c9efaf2a
BakhodiribnYashinibnMansur <phapp0224mb@gmail.com>
2022-04-20 04:48:09 +0500

    implemented auth middleware

--- package/handler/list.go
+++ package/handler/list.go
@@ -1,8 +1,16 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func (h *Handler) createList(c *gin.Context) {
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) createList(ctx *gin.Context) {
+	id, _ := ctx.Get(userCtx)
+	ctx.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 
 }
 
