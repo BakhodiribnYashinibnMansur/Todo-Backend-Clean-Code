d7af3f82f0030de711aae7fb15b007b7d6bb7586
BakhodiribnYashinibnMansur <phapp0224mb@gmail.com>
2022-04-20 06:01:02 +0500

    add shutdown

--- command/main.go
+++ command/main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
 	"todocc/config"
 	"todocc/package/handler"
 	"todocc/package/repository"
 	"todocc/package/service"
 	"todocc/server"
-	"todocc/util/logrus"
 
 	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	logger := logrus.GetLogger()
+	// logger := logrus.GetLogger()
 
 	configs, err := config.InitConfig()
 	if err != nil {
-		logger.Fatalf("error initializing configs: %s", err.Error())
+		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
 
 	db, err := repository.NewPostgresDB(repository.Config{
@@ -28,10 +33,10 @@ func main() {
 		Password: configs.DBPassword,
 	})
 
-	logger.Infof("configs: %v", configs)
+	logrus.Infof("configs: %v", configs)
 
 	if err != nil {
-		logger.Fatalf("failed to initialize db: %s", err.Error())
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
 	}
 
 	repos := repository.NewRepository(db)
@@ -39,7 +44,25 @@ func main() {
 	handlers := handler.NewHandler(services)
 
 	srv := new(server.Server)
-	if err := srv.Run(configs.ServerPort, handlers.InitRoutes()); err != nil {
-		logger.Fatalf("error occurred while running http server: %s", err.Error())
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error occured while running http server: %s", err.Error())
+		}
+	}()
+
+	logrus.Print("TodoApp Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("TodoApp Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occured on server shutting down: %s", err.Error())
+	}
+
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occured on db connection close: %s", err.Error())
 	}
 }
